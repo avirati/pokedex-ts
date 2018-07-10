7b00765d9f0217033b4b06a2bfc77507230481ff
Avinash Verma <avinashv@flock.co>
2018-07-10 11:55:11 +0530

    Render pokemon stats

--- src/core/Interfaces.ts
+++ src/core/Interfaces.ts
@@ -10,8 +10,28 @@ export interface IPokeCardProps {
   pokemonData: IPokemon;
 }
 
-export interface IPokemonResearchData {
+interface IPokemonMoves {
+  move: {
+    name: string;
+  };
+}
 
+interface IPokemonStats {
+  stat: {
+    name: string;
+  };
+  base_stat: number;
+}
+
+interface IPokemonResearchData {
+  sprites: {
+    front_default: string;
+  };
+  base_experience: number;
+  height: number;
+  weight: number;
+  stats: IPokemonStats[];
+  moves: IPokemonMoves[];
 }
 
 export interface IPokemon {
--- src/core/poke-card/PokeCardComponent.tsx
+++ src/core/poke-card/PokeCardComponent.tsx
@@ -7,22 +7,16 @@ import './PokeCard.css';
 export default class PokeCardComponent extends React.Component <IPokeCardProps> {
   public render() {
     const { pokemonData } = this.props;
-    if (pokemonData.researchData) {
+    const pData = pokemonData.researchData;
+    if (pData) {
       return (
         <div className='row poke-card'>
           <div
-            className='col s12 research-card'
-            ng-if='pokemon.researchData'
-            ng-init='pData = pokemon.researchData'
-          >
+            className='col s12 research-card'>
             <div className='image-container'>
               <div className='row'>
                 <div className='col s12'>
-                  <img
-                    ng-src='{{pData.sprites.front_default}}'
-                    alt=''
-                    className='pokemon-avatar'
-                  />
+                  <img src={pData.sprites.front_default} className='pokemon-avatar'/>
                 </div>
               </div>
             </div>
@@ -30,41 +24,53 @@ export default class PokeCardComponent extends React.Component <IPokeCardProps> 
               <div className='row margin-bottom--0'>
                 <div className='col s12'>
                   <label>Name: </label>
-                  <span>Pokemon dot Name</span>
+                  <span>{pokemonData.name}</span>
                 </div>
               </div>
               <div className='row margin-bottom--0'>
                 <div className='col s12'>
                   <label>XP: </label>
-                  <span>Pokemon dot base_experience</span>
+                  <span>{pData.base_experience}</span>
                 </div>
               </div>
               <div className='row margin-bottom--0'>
                 <div className='col s12'>
                   <label>Height: </label>
-                  <span>Pokemon dot height</span>
+                  <span>{pData.height}</span>
                 </div>
               </div>
               <div className='row margin-bottom--0'>
                 <div className='col s12'>
                   <label>Weight: </label>
-                  <span>Pokemon dot weight</span>
+                  <span>{pData.weight}</span>
                 </div>
               </div>
               <div
                 className='row margin-bottom--0'
                 ng-repeat='obj in pData.stats'
               >
-                <div className='col s12'>
-                  <label>Obj dot stat dot name</label>
-                  <span>Obj dot base_stat</span>
-                </div>
+                {
+                pData.stats.map((obj, index) => {
+                  return (
+                    <div className='col s12' key={index}>
+                      <label>{obj.stat.name}</label>
+                      <span>{obj.base_stat}</span>
+                    </div>
+                  );
+                })
+              }
               </div>
               <div className='row margin-bottom--0 only-desktop moves-list'>
                 <div className='col s12'>
-                  <div className='pill' ng-repeat='obj in pData.moves'>
-                    Obj dot move dot name
-                  </div>
+                {
+                  pData.moves.map((obj, index) => {
+                    return (
+                      <div className='pill' key={index}>
+                        {obj.move.name}
+                      </div>
+                    );
+                  })
+                }
                 </div>
               </div>
             </div>
