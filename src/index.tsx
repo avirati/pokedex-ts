6b13cd3183d7a3336ef7de1e31f265064d30794a
Avinash Verma <avinashv@flock.co>
2018-07-14 21:58:10 +0530

    Using redux-persistor to persist pokeList

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
-import rootSaga from './core/sagas';
 
 import App from './App';
 import { IPokemon } from './core/Interfaces';
 import reducer from './core/reducer';
+import rootSaga from './core/sagas';
 import { composeEnhancers } from './core/utils';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -18,15 +21,25 @@ const db: IPokemon[] = require('./dump.json');
 const sagaMiddleware = createSagaMiddleware();
 
 function configureStore(initialState?: object) {
+  const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['pokeList'],
+  };
+
+  const persistedReducer = persistReducer(persistConfig, reducer);
   // configure middlewares
   const middlewares = [sagaMiddleware];
   // compose enhancers
   const enhancer = composeEnhancers(applyMiddleware(...middlewares));
   // create store
-  return createStore(reducer, initialState!, enhancer);
+  const store = createStore(persistedReducer, initialState!, enhancer);
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 }
 
-const store = configureStore({
+const { store, persistor } = configureStore({
   pokeList: db,
 });
 
@@ -34,7 +47,9 @@ sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 document.getElementById('root') as HTMLElement);
 registerServiceWorker();
